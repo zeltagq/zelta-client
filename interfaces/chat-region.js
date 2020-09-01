c3a1a975da9b5a65d1226f486ccef0ad336ecfa2
zaygozi <karmarex@xetastorm.localdomain>
2020-09-01 21:25:19 +0530

    Version 1.0.0

--- interfaces/chat-region.js
+++ interfaces/chat-region.js
@@ -4,11 +4,11 @@ const chalk = require('chalk');
 function chatRegion(value) {
     if (value.toLowerCase() === 'asia') {
         config.set('chat-url', 'https://as.chat.zelta.gq');
-        cosnole.log(chalk.yellowBright('[Zelta Chat] Server : Asia (India)'));
+        console.log(chalk.yellowBright('[Zelta Chat] Server : Asia (India)'));
     }
     else if (value.toLowerCase() === 'europe') {
         config.set('chat-url', 'https://eu.chat.zelta.gq');
-        cosnole.log(chalk.yellowBright('[Zelta Chat] Server : Europe (Ireland)'));
+        console.log(chalk.yellowBright('[Zelta Chat] Server : Europe (Ireland)'));
     }
     else {
         console.log(chalk.yellowBright('This region currently does not host a chat server!'));
